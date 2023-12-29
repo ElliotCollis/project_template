179792dc02c84db5727463247d91fc572da03b91
Elliot <elliot.collis@gmail.com>
2023-12-29 13:17:45 +0900

    change : util folders

---
+++ Assets/Root/Code/Scripts/Utilities/DataUtil/AssetData.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace HowlingMan
+{
+    public static class AssetData
+    {
+        public const string MainMenuPrefab = "MainMenuPrefab";
+        public const string SplashScreensPrefab = "SplashScreensPrefab";
+        public const string CustomizationPrefab = "CustomizationPrefab";
+        public const string OptionsPrefab = "OptionsPrefab";
+        public const string ShopPrefab = "ShopPrefab";
+        public const string PauseMenuPrefab = "PauseMenuPrefab";
+        public const string UISimpleButton = "UISimpleButton";
+        public const string MainMenuHeaderPrefab = "MainMenuHeaderPrefab";
+        public const string MainMenuFooterPrefab = "MainMenuFooterPrefab";
+        public const string PopUpDialogueBox = "PopUpDialogueBox";
+        public const string PopUpDialogueBoxLarge = "PopUpDialogueBoxLarge";
+        public const string TabPanelContainer = "TabPanelContainer";
+        public const string LargePanel = "LargePanel";
+    }
+}
---
+++ Assets/Root/Code/Scripts/Utilities/DataUtil/GlobalData.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace HowlingMan
+{
+    public static class GlobalData
+    {
+        public const float fadeTimes = 0.4f;
+
+        public const int dialogueSmallSizeLimit = 60;
+        
+    }
+}
