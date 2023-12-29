43c466c7d01fcd4e72f260d328d4833f6dc8be61
Elliot <elliot.collis@gmail.com>
2023-12-29 13:20:42 +0900

    add : tab menu

---
+++ Assets/Root/Code/Scripts/UI/Components/PanalRef.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace HowlingMan.UI
+{
+    public class PanalRef : MonoBehaviour
+    {
+        public CanvasGroup canvasGroup;
+
+        public HorizontalLayoutGroup layoutGroup;
+
+        public Transform content;
+
+        public ScrollRect scroll;
+    }
+}
---
+++ Assets/Root/Code/Scripts/UI/Components/TabController.cs
@@ -0,0 +1,94 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace HowlingMan.UI
+{
+    [SerializeField]
+    public class TabController
+    {
+        public PanalRef basePanel;
+        public List<ButtonRef> tabButtons;
+        public List<CanvasGroup> tabPanels;
+
+        public int currentPanelIndex = -1;
+
+        private AddressableAssetLoader assetLoader;
+
+        public TabController()
+        {
+            assetLoader = new AddressableAssetLoader();
+        }
+
+        public async Task InitializeAsync(Transform parent, string[] buttonNames, string[] panelAssets, int startPanelIndex = 0)
+        {
+            await SpawnBase(parent);
+            await SpawnTabsAndPanels(buttonNames, panelAssets);
+            DisplayPanel(startPanelIndex);
+        }
+
+        private async Task SpawnBase(Transform parent)
+        {
+            GameObject basePanelPrefab = await assetLoader.LoadAssetAndInstantiateAsync<GameObject>(AssetData.LargePanel, parent);
+            basePanel = basePanelPrefab.GetComponent<PanalRef>();
+        }
+
+        private async Task SpawnTabsAndPanels(string[] buttonNames, string[] panelAssets)
+        {
+            var tasks = new List<Task<GameObject>>();
+            tasks.Add(assetLoader.LoadAssetAsync<GameObject>("UISimpleButton"));
+
+            foreach (var panelName in panelAssets)
+            {
+                tasks.Add(assetLoader.LoadAssetAndInstantiateAsync<GameObject>(panelName, basePanel.content));
+            }
+
+            var results = await Task.WhenAll(tasks);
+
+            SetupTabs(buttonNames, results[0]);
+            SetupPanels(results, panelAssets.Length);
+        }
+
+        private void SetupTabs(string[] buttonNames, GameObject buttonPrefab)
+        {
+            tabButtons = new List<ButtonRef>();
+            for (int i = 0; i < buttonNames.Length; i++)
+            {
+                GameObject button = GameObject.Instantiate(buttonPrefab, basePanel.layoutGroup.transform);
+                ButtonRef buttonRef = button.GetComponent<ButtonRef>();
+
+                int index = i;  // Local variable to capture the current index
+                buttonRef.AddButtonAction(() => DisplayPanel(index));
+                tabButtons.Add(buttonRef);
+            }
+
+            assetLoader.UnloadAsset(buttonPrefab);
+        }
+
+
+        private void SetupPanels(GameObject[] panelAssets, int numberOfPanels)
+        {
+            tabPanels = new List<CanvasGroup>();
+            for (int i = 0; i < numberOfPanels; i++)
+            {
+                CanvasGroup canvasGroup = panelAssets[i + 1].GetComponent<CanvasGroup>(); // +1 to skip the button prefab
+                tabPanels.Add(canvasGroup);
+                canvasGroup.gameObject.SetActive(false);
+            }
+        }
+
+        private void DisplayPanel(int index)
+        {
+            if (currentPanelIndex >= 0)
+            {
+                tabPanels[currentPanelIndex].gameObject.SetActive(false);
+            }
+
+            tabPanels[index].gameObject.SetActive(true);
+            currentPanelIndex = index;
+        }
+    }
+}
+
+//  spawing everything in finally, but not set sizes correctly, so need to run through and set sizes and pivot points to center.
+//  also most things are spawwning backwards...?
